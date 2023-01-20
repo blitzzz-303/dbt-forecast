a4b1f7db9e98d60e7d7e07affbd97994488246e0
HT <hung@infinitelambda.com>
2023-01-21 00:14:01 +0700

    resolve duplication in joins

--- models/int/INT_SALES_DS_EVALUATE_COUNTRY.sql
+++ models/int/INT_SALES_DS_EVALUATE_COUNTRY.sql
@@ -8,4 +8,3 @@ from {{ref('INT_SALES_DS_WEEKLY')}} w
 join {{ref('INT_SALES_DS_PREDICT_COUNTRY')}} p
     on w.store_dept_pk = p.store_dept_pk
     and w.WEEK_DATE = p.WEEK_DATE::varchar::date
-where w.Store in (1, 2) and w.Dept = 1
